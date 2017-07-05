069130f558f8d6ecf3313245ca8c1b89ee39c0e5
Dave Wolf <dave45678@gmail.com>
2017-07-05 15:16:40 -0400

    updated with more comments

--- src/Counter.java
+++ src/Counter.java
@@ -1,4 +1,15 @@
-public class Counter implements Runnable {
+/***
+ * 
+ * @author oracle
+ * The counter class implements the Runnable interface which means that 
+ * you can extend another class or implement other interfaces. This gives you greater 
+ * flexibility than extending the Thread class.
+ * 
+ * The interface requires you to override the run() method (no arguments).
+ * Your code goes into run. 
+ */
+public class Counter 
+		implements Runnable {
 
 	
 	@Override
@@ -6,18 +17,19 @@ public class Counter implements Runnable {
 		Thread ct = Thread.currentThread();
 		int count = 1;
 		System.out.println("Timer begins");
-		// 
-		//while(!ct.isInterrupted()) {
+
 		while(true) {
 			try {
+				System.out.println("sleeping for 1 second");
 				Thread.sleep(1000); // Sleep for 1 second
 			} catch (InterruptedException e) {
+				System.out.println("Interrupted Exception Caught!");
 				break;
 			}
 			System.out.println("Elapsed Time: " + count + " seconds");
 			count++;
 		}
-		System.out.println("User paused the timer");
+		System.out.println("User interrupted the thread");
 
 	}
 
--- src/ThreadInterrupt.java
+++ src/ThreadInterrupt.java
@@ -3,16 +3,23 @@ import java.util.Scanner;
 public class ThreadInterrupt {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
 			System.out.println("Press Enter Key to stop the timer");
+			
+			//create an instance of the thread and start it by calling start()
+			//the run method will automatically execute.
 			Thread counter = new Thread(new Counter());
 			counter.start();
+			
 			Scanner keyboard = new Scanner(System.in);
 			String s = "start";
+			//s will remain equal to "start" within the loop
+			//until the user hits enter
 			while(!s.equals("")) {
 				s = keyboard.nextLine();
 			}
+			
+			//the interrupt method is part of thread and will execute when
+			//the user causes the while() loop to exit.
 			counter.interrupt();
 			keyboard.close();
 	}
